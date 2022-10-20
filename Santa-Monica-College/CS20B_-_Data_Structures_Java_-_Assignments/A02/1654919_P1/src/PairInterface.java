ead8732c8e8ea7927f9fc14801286f13e581e270
Jonathan Reznik <22972886+jonathanreznik1@users.noreply.github.com>
2022-10-19 22:25:44 -0700

    Changes to files

--- Santa-Monica-College/CS20B_-_Data_Structures_Java_-_Assignments/A02/1654919_P1/src/PairInterface.java
+++
@@ -1,7 +0,0 @@
-//The interface for Pairs has two setter and getters required
-interface PairInterface<T> {
-	void setFirst(T first);
-	void setSecond(T second);
-	T getFirst();
-	T getSecond();
-}
--- Santa-Monica-College/CS85_-_PHP_-_Assignments/PHPcode/TrimStrings.php
+++
@@ -1,7 +0,0 @@
-<?php
-$string_name='   Welcome to My Webpage ';
-$new_string=trim($string_name);
-echo $new_string."<br />";
-var_dump($new_string);
-
-?>
